a39456fb2e6cde52a67a66aee788b8ba73c1697a
lmorgan2398 <lmorgan2398@gmail.com>
2026-06-30 19:22:48 +0000

    Add transform functions, change number inputs to objects
    Transform functions will modify number inputs in place once
    the operation is executed. To store the transform type until
    operation execution, number inputs are now objects with value and
    transform values

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 console.log('hello');
 
-let firstNumber = '';
-let secondNumber;
-let operator;
+let firstNumber = {
+    value: '',
+    transform: '',
+    transformValue: ''
+};
+
+let secondNumber = {
+    value: '',
+    transform: '',
+    transformValue: ''
+};
+
+let operator = '';
+
+let trigMode = 'deg';
 
 const displayContainer = document.querySelector('.display-container');
 
@@ -15,12 +27,35 @@ buttonsContainer.addEventListener('click', (event) => {
     }
 })
 
-const math = (() => {
+const operation = (() => {
+
     const add = (first, second) => { return first + second };
     const subtract = (first, second) => { return first - second };
     const multiply = (first, second) => { return first * second };
     const divide = (first, second) => { return first / second };
-    const exponent = (first, second) => { return first ** second };
 
-    return { add, subtract, multiply, divide, exponent };
+    return { add, subtract, multiply, divide };
+
+})();
+
+const transform = (() => {
+
+    const exponent = (first, second) => { return first ** second };
+    const squareRoot = (num) => { return Math.sqrt(num) };
+
+    const degToRad = (num) => { return num * ( Math.PI / 180 ) };
+
+    const sin = (num) => { return Math.sin(num) };
+    const cos = (num) => { return Math.cos(num) };
+    const tan = (num) => { return Math.tan(num) };
+
+    const asin = (num) => { return Math.asin(num) };
+    const acos = (num) => { return Math.acos(num) };
+    const atan = (num) => { return Math.atan(num) };
+
+    const ln = (num) => { return Math.log(num) };
+    const log = (num) => { return Math.log10(num) };
+
+    return { exponent, squareRoot, degToRad, sin, cos, tan, asin, acos, atan, ln, log };
+
 })();
